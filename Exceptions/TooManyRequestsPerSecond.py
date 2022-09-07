b864cf9f9ebdd0af812d74e235d38c6bcb72e0ab
Aziz <ilkhomzhonmatazimov@gmail.com>
2022-09-07 17:52:02 +0500

    bug fixes

--- Exceptions/TooManyRequestsPerSecond.py
+++ Exceptions/TooManyRequestsPerSecond.py
@@ -1,4 +1,4 @@
 class TooManyRequestsPerSecond(Exception):
-    def __init__(self, text='Too many'):
+    def __init__(self, text='Слишком много запросов в секунду'):
         self._text = text
         super().__init__(self._text)
